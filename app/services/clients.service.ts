7e0c80ac17333d751d98c0a2bed51453625b46c9
fenrils <nestor-91-gh@hotmail.com>
2021-09-21 01:05:59 +0200

    1 file modified

--- app/services/clients.service.ts
+++ app/services/clients.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -7,6 +7,11 @@ export class ClientsService {
   constructor(private http: HttpClient) {}
 
   addClient(user: any): Observable<any> {
-    return this.http.post('https://vtwhny.deta.dev/addClient/', user);
+    console.log(JSON.stringify(user));
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    let options = { headers: headers };
+    return this.http.post('https://vtwhny.deta.dev/addClient/', user, options);
   }
 }
